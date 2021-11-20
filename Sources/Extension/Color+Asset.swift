0b3b35ef1b63b9e719f42f31707d894e62d5ad34
kimxwan <kimxwan0319@naver.com>
2021-11-20 22:53:27 +0900

    Color Extension 옵셔널 강제 해제

--- Sources/Extension/Color+Asset.swift
+++ Sources/Extension/Color+Asset.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 extension UIColor {
-    static let keyColor = UIColor(named: "KeyColor")
-    static let bgColor = UIColor(named: "BGColor")
-    static let black = UIColor(named: "Black")
-    static let cardColor = UIColor(named: "CardColor")
-    static let gray02 = UIColor(named: "Gray02")
-    static let gray03 = UIColor(named: "Gray03")
-    static let runningType01 = UIColor(named: "RunningType01")
-    static let runningType02 = UIColor(named: "RunningType02")
+    static let keyColor = UIColor(named: "KeyColor")!
+    static let bgColor = UIColor(named: "BGColor")!
+    static let black = UIColor(named: "Black")!
+    static let cardColor = UIColor(named: "CardColor")!
+    static let gray02 = UIColor(named: "Gray02")!
+    static let gray03 = UIColor(named: "Gray03")!
+    static let runningType01 = UIColor(named: "RunningType01")!
+    static let runningType02 = UIColor(named: "RunningType02")!
 }
