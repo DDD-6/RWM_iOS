61071fc312f0418a0256c6ebf41e4b516c31872d
kimxwan <kimxwan0319@naver.com>
2021-11-20 22:15:06 +0900

    color 리소스 관리를 위한 Extension 추가

---
+++ Sources/Extension/Color+Asset.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Asset.swift
+//  RWM
+//
+//  Created by 김수완 on 2021/11/20.
+//  Copyright © 2021 com.6hc. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let keyColor = UIColor(named: "KeyColor")
+    static let bgColor = UIColor(named: "BGColor")
+    static let black = UIColor(named: "Black")
+    static let cardColor = UIColor(named: "CardColor")
+    static let gray02 = UIColor(named: "Gray02")
+    static let gray03 = UIColor(named: "Gray03")
+}
